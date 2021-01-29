2830ba4fb741daf480b17aca2c512665b59ba6e9
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-29 14:24:36 +0200

    Pass tests

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,48 @@
 class Calculator
-  def add(number_1, number_2)
-    number_1 + number_2
-  end  
+
+  def initialize(operater)
+    @operater = operater
+    @locked_add = false
+    @locked_multiplication = false
+    @locked_square = false
+  end
+
+  def add(num1, num2)
+   return @operater.add(num1, num2) unless @locked_add
+   :forbidden
+  end
+
+  def multiply(number_1, number_2)
+   return @operater.multiply(number_1, number_2) unless @locked_multiplication
+   :multiplication_forbidden
+  end
+
+  def square(number) 
+    return @operater.square(number) unless @locked_square
+    :square_forbidden
+  end
+
+  def unlock_add
+    @locked_add = true
+  end
+
+  def locked_add?
+    @locked_add
+  end
+
+  def lock_multiply
+    @locked_multiplication = true
+  end
+
+  def locked_multiplied?
+    @locked_multiplication
+  end
+
+  def lock_square
+    @locked_square = true
+  end
+
+  def locked_square?
+    @locked_square
+  end
 end
--- calculator_test.rb
+++ calculator_test.rb
@@ -1,15 +1,181 @@
+require 'simplecov'
+SimpleCov.start
 require './test'
 require './calculator.rb'
 
-test "that it adds two numbers" do
+class Adder
+  def add(num1, num2)
+    @called_with = num1 + num2
+    "happy message"
+  end
+
+  def what_was_add_called_with
+    @called_with
+  end
+end
+
+class Multiplier
+  def multiply(num1, num2)
+    @called_with = num1 * num2
+    "multiplier message"
+  end
+
+  def what_was_multiply_called_with
+    @called_with
+  end
+end
+
+class Squarer
+  def square(number)
+    @called_with = number**2
+    "square completed"
+  end
+
+  def what_was_square_called_with
+    @called_with
+  end
+end
+
+test "that it starts off unlocked" do
+  adder = Adder.new
+  calculator = Calculator.new(adder)
+
+  result = calculator.locked_add?
+
+  assert_equal(result, false)
+end
+test "that it can prevent from adding" do
+  # Arrange
+  adder = Adder.new
+  calculator = Calculator.new(adder)
+  calculator.unlock_add
+
+  # Act
+  result = calculator.locked_add?
+
+  # Assert
+  assert_equal(result, true)
+end
+
+test "that it adds two numbers when unlocked" do
   # Arrange - set up the situation to test within
-  calculator = Calculator.new
-  number_1 = 1
-  number_2 = 1
-  
-  # Act - call the behaviour we want to test
-  result = calculator.add(number_1, number_2)
+  adder = Adder.new
+  calculator = Calculator.new(adder)
+
+  # Act - call the behaviour we want to test 
+  result = calculator.add(1, 1)
 
   # Assert - check the result is what we expect
-  assert_equal(result,  2)
+  assert_equal(result,  "happy message")
+  assert_equal(adder.what_was_add_called_with, 2)
+end
+
+test "that it does not add when locked" do
+  # Arrange - set up the situation to test within
+  adder = Adder.new
+  calculator = Calculator.new(adder)
+  calculator.unlock_add
+  # Act - call the behaviour we want to test
+  result = calculator.add(1, 1)
+
+  # Assert - check the result is what we expect
+  assert_equal(result,  :forbidden)
+  assert_equal(adder.what_was_add_called_with, nil)
+end
+
+test "that it starts off unlocked" do
+  multiplier = Multiplier.new
+  calculator = Calculator.new(multiplier)
+
+  result = calculator.locked_multiplied?
+
+  assert_equal(result, false)
+end
+
+test "that it can prevent from multiplying" do
+  # Arrange - set up the situation to test within
+  multiplier = Multiplier.new
+  calculator = Calculator.new(multiplier)
+  calculator.lock_multiply
+
+  result = calculator.locked_multiplied?
+
+  assert_equal(result, true)
+end
+test "that it multiplies two numbers when unlocked" do
+  # Arrange - set up the situation to test within
+  multiplier = Multiplier.new
+  calculator = Calculator.new(multiplier)
+
+  # Act - call the behaviour we want to test
+  result = calculator.multiply(2, 5)
+
+  # Assert - check the result is what we expect
+  assert_equal(result,  "multiplier message")
+  assert_equal(multiplier.what_was_multiply_called_with,  10)
+end
+
+test "that it does not multiply when locked" do
+   # Arrange - set up the situation to test within
+   multiplier = Multiplier.new
+   calculator = Calculator.new(multiplier)
+   calculator.lock_multiply
+
+   # Act - call the behaviour we want to test
+   result = calculator.multiply(2, 5)
+
+   # Assert - check the result is what we expect
+   assert_equal(result,  :multiplication_forbidden)
+   assert_equal(multiplier.what_was_multiply_called_with, nil)
+end
+
+test "that it stars off unlocked" do
+  # Arrange - set up the situation to test within
+  squarer = Squarer.new
+  calculator = Calculator.new(squarer)
+
+   # Act - call the behaviour we want to test
+   result = calculator.locked_square?
+
+   # Assert - check the result is what we expect
+  assert_equal(result,  false)
+end
+
+test "that it can lock" do
+  # Arrange - set up the situation to test within
+  squarer = Squarer.new
+  calculator = Calculator.new(squarer)
+  calculator.lock_square
+
+  # Act - call the behaviour we want to test
+  result = calculator.locked_square?
+
+  # Assert - check the result is what we expect
+  assert_equal(result,  true)
+end
+test "that it returns a squared number when unlocked" do
+  # Arrange - set up the situation to test within
+  squarer = Squarer.new
+  calculator = Calculator.new(squarer)
+
+  # Act - call the behaviour we want to test
+  result = calculator.square(3)
+
+  # Assert - check the result is what we expect
+  assert_equal(result, "square completed")
+  assert_equal(squarer.what_was_square_called_with,  9)
+end
+
+test "that it does not return a squared number when locked" do
+  # Arrange - set up the situation to test within
+  squarer = Squarer.new
+  calculator = Calculator.new(squarer)
+  calculator.lock_square
+
+  # Act - call the behaviour we want to test
+  result = calculator.square(3)
+  
+  # Assert - check the result is what we expect
+  assert_equal(result, :square_forbidden)
+  assert_equal(squarer.what_was_square_called_with,  nil)
 end
